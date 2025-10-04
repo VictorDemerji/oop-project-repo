f91df7ea4ec49d519069a4c73bc1d1684245a402
VictorDemerji <demerji.v1@gmail.com>
2025-10-04 13:19:40 +0300

    Add new object

--- src/MyClass.java
+++ src/MyClass.java
@@ -1,9 +1,15 @@
 public class MyClass {
     public static void main(String[] args) {
         Point p = new Point();
+        Point p2 = new Point();
+
         p.x = 10;
         p.y = 20;
 
+        p2.x = 30;
+        p2.y = 40;
+
         System.out.println(p.x + " " + p.y);
+        System.out.println(p2.x + " " + p2.y);
     }
 }
