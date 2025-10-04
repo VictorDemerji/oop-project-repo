a8477a478a8d231eb0ca57965d73290e927fb728
VictorDemerji <demerji.v1@gmail.com>
2025-10-04 14:14:01 +0300

    Add pack3

--- src/pack3/Class1.java
+++ src/pack3/Class1.java
@@ -1,5 +1,6 @@
 package pack3;
 
 public class Class1 {
-
+    String nume, prenume, hobby;
+    int varsta;
 }
---
+++ src/pack3/Class2.java
@@ -0,0 +1,16 @@
+package pack3;
+
+public class Class2 {
+    public static void main(String[] args) {
+        Class1 obj1 = new Class1();
+        obj1.nume    = "Demerji";
+        obj1.prenume = "Victor";
+        obj1.varsta  = 19;
+        obj1.hobby   = "Fotbal";
+
+        System.out.println(obj1.nume);
+        System.out.println(obj1.prenume);
+        System.out.println(obj1.varsta);
+        System.out.println(obj1.hobby);
+    }
+}
